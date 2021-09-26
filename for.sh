3c06f5b56b74fc53c07a86a2691f2954ec844244
eheperson <barannkuzu@gmail.com>
2021-09-26 13:35:12 +0300

    if and for ecamples are included

---
+++ for.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# for VARIABLE in 1 2 3 4 5 .. N
+# do
+# 	command1
+# 	command2
+# 	commandN
+# done
+# --------
+# for VARIABLE in file1 file2 file3
+# do
+# 	command1 on $VARIABLE
+# 	command2
+# 	commandN
+# done
+# --------
+# for OUTPUT in $(Linux-Or-Unix-Command-Here)
+# do
+# 	command1 on $OUTPUT
+# 	command2 on $OUTPUT
+# 	commandN
+# done
+#
+#
+for i in 1 2 3 4 5
+do
+   echo "Welcome $i times"
+done
+#
+#
+for i in {1..10}
+do
+   echo "Welcome $i times"
+done
+#
+#
+echo "Bash version ${BASH_VERSION}..."
+for i in {0..10..2}
+  do 
+     echo "Welcome $i times"
+ done
---
+++ if.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# if [condition]
+# then
+#     block_of_statements
+# fi
+# --------
+# if [condition]
+# then
+#     block_of_statements
+# else
+#     block_of_statements
+# fi
+# --------
+# if [condition1]
+# then
+#     block_of_statements
+# elif [condition2]
+# then
+#     block_of_statements
+# elif [condition3]
+# then
+#     block_of_statements
+# else
+#     block_of_statements
+# fi
